115ff51f17738d7046338f160d537a2c9e66db73
dylan0stewart <57077858+dylan0stewart@users.noreply.github.com>
2020-02-05 16:45:31 -0500

    Add files via upload
    adding model to app

--- api/application.py
+++ api/application.py
@@ -5,24 +5,12 @@ from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from .models import DB
 import pickle
-'''
-import gensim
-import gensim.models.doc2vec as doc2vec
-from gensim.models.doc2vec import Doc2Vec
-from gensim.models.doc2vec import TaggedDocument
 import re
 import os
-from dotenv import load_dotenv
 import sqlite3
 import praw
-'''
-'''
-import ml model here in this format
-modeltype = pickle.load(open('#name of model#.pkl', 'rb'))
-model_dbow = Doc2Vec.load('#doc2vector model#.pkl')
+import spacy
 
-
-'''
 
 def create_app():
     APP = Flask(__name__)
@@ -37,14 +25,14 @@ def create_app():
 
     return APP
 
-    '''
+    
     @APP.route('/predict', methods=['POST'])
     def predict(): #define a prediction function
         lines = request.get_json(force=True) # read in json data
         body = lines['body'] # get variable
 
         #deserialize model
-        with open('model.pickle', 'rb') as mod:
+        with open('test_model.pkl', 'rb') as mod:
             model = pickle.load(mod)
 
         #actual prediction
@@ -58,12 +46,12 @@ def create_app():
             }
     
         # give output to sender.
-        return app.response_class(
+        return APP.response_class(
             response=json.dumps(send_back),
             status=200,
             mimetype='application/json'
         )
-    '''
+    
 
 
 
